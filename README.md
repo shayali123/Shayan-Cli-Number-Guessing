ef3d114234e7b5f8907faa9032b510c052d429fe
shayali123 <160710648+shayali123@users.noreply.github.com>
2024-03-23 13:33:37 +0500

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+"# Shayan-cli-number-guessing" 
---
+++ lec5.ts
@@ -0,0 +1,17 @@
+import inquirer from "inquirer"
+
+let random = Math.floor(Math.random() * 10 + 1);
+// console.log(random)
+const answer = await inquirer.prompt([
+    {
+        name: "userguessednumber",
+        type: "number",
+        message: "Please guess a number 1-10 :",
+    },
+]);
+
+if(answer.userguessednumber === random) {
+    console.log("Congratulation, you guessed a right number")
+} else{
+    console.log("You guessed a wrong number")
+}
